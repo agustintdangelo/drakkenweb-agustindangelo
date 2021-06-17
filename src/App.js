9424936f10b21dc8620380d82d9200fa1d82aa7f
agustintdangelo <agustintdangelo@gmail.com>
2021-06-17 17:58:05 -0300

    NavBar y componente de ItemListContainer con una greeting integradas a la app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import logoDrakken from './logo.svg';
+import NavBar from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Drakken
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <NavBar logo={logoDrakken}/>
+        </header>
+        <div>
+          <ItemListContainer greeting='Bienvenidos a Drakken, donde encontrás lo que querés para tu pc.'/>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
