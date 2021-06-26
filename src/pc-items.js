f0a807bb054e46bebfe61468248be7107cc703ee
agustintdangelo <agustintdangelo@gmail.com>
2021-06-26 03:17:13 -0300

    Create pc-items.js

---
+++ src/pc-items.js
@@ -0,0 +1,26 @@
+export const pcItems =[
+{
+    id:0,
+    title:"Razer Viper Mini",
+    description:"El ratón Razer Viper Mini, nacido para llevar hasta el límite el juego ultraligero, es ahora de menor tamaño sin sacrificar la grandeza de su rendimiento.",
+    category:"Mouse",
+    price:"$5000",
+    pictureUrl:"https://app.contabilium.com/files/explorer/7026/Productos-Servicios/concepto-3346007.jpg"
+},
+{
+    id:1,
+    title:"Redragon YAMA K550",
+    description:"El Redragon K550 YAMA tiene ultima tecnología y está lleno de características premium." ,
+    category:"Teclado",
+    price:"$8000" ,
+    pictureUrl:"https://i1.wp.com/www.pcmrace.com/wp-content/uploads/2017/05/K550WFEATURED.png?resize=1109%2C520&ssl=1" 
+},
+{
+    id:2 ,
+    title:"Procesador Ryzen 5 1600 AF" ,
+    description:"El procesador para computadoras de escritorio de 12 subprocesos y 6 núcleos de menor consumo de energía de todo el mundo con verdadera inteligencia artificial" ,
+    category:"Procesador",
+    price: "$20000",
+    pictureUrl: "https://mla-s1-p.mlstatic.com/696005-MLA42015175424_052020-F.jpg"
+}
+];
